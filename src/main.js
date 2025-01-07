c84400dfaf8dfab3f197d1bc4440542c0292264d
Young Seo <youngseokim7@gmail.com>
2025-01-07 14:09:34 -0500

    initial use of weather api

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import './styles.css';
+console.log('hello world!');
+
+async function getData(){
+    const apiFetch = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/singapore?unitGroup=metric&key=9VKUDM7Q8M8GXRWBKGD9F9MMZ&contentType=json');
+    const data = await apiFetch.json(); 
+    console.log(data);
+    let daysArray = data.days;
+    for (let i = 0; i < daysArray.length; i++){
+        console.log(daysArray[i].tempmax);
+    }
+}
+
+getData();
