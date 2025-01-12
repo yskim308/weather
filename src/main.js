89888998cd39ba3742d7d97733f0449630857e8d
Young Seo <youngseokim7@gmail.com>
2025-01-12 09:25:44 +0900

    create search input with api

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import './styles.css';
 console.log('hello world!');
 
-async function getData(){
-    const apiFetch = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/singapore?unitGroup=metric&key=9VKUDM7Q8M8GXRWBKGD9F9MMZ&contentType=json');
+const searchForm = document.querySelector('#locationSearch');
+searchForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    let locationString = document.querySelector('#locationString').value;
+    console.log(locationString)
+    event.target.reset();
+
+    getData(locationString);
+});
+
+
+async function getData(locationString){
+    const apiFetch = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationString}?unitGroup=metric&key=9VKUDM7Q8M8GXRWBKGD9F9MMZ&contentType=json`, {mode: 'cors'});
     const data = await apiFetch.json(); 
     console.log(data);
     let daysArray = data.days;
@@ -11,4 +22,3 @@ async function getData(){
     }
 }
 
-getData();
