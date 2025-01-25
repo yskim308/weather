3b35418df8098903e2c6f476fbe16a1de1f87455
Young Seo Kim <youngseokim7@gmail.com>
2025-01-25 20:31:56 +0900

    complete updating weather conditions for current day

--- src/dom.js
+++ src/dom.js
@@ -14,4 +14,31 @@ function updateTodayIcon(todayObject){
     todayIcon.src = images[todayObject.icon];
 }
 
-export {updateTodayIcon}
+function updateTodayTemperature(todayObject){
+    const temperature = document.querySelector('#temperature');
+    temperature.innerText = todayObject.temp;
+}
+
+function updateTodayConditions(todayObject){
+    const todayConditions = document.querySelector('#todayConditions')
+    todayConditions.children[0].innerText = `precipitation: ${todayObject.precip}`;
+    todayConditions.children[1].innerText = `humidity: ${todayObject.humidity}`;
+    todayConditions.children[2].innerText = `uv index: ${todayObject.uvindex}`;
+}
+
+function updateTodayDescription(todayObject){
+    const todayDescription = document.querySelector('#todayDescription');
+    todayDescription.innerText = todayObject.description;
+}
+
+function updateToday(todayObject){
+    if (!todayObject){
+        console.error('Invalid object passed to updateToday function');
+        return;
+    }
+    updateTodayIcon(todayObject);
+    updateTodayTemperature(todayObject);
+    updateTodayConditions(todayObject);
+    updateTodayDescription(todayObject);
+}
+export {updateToday};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import { updateTodayIcon } from './dom';
+import { updateToday } from './dom';
 console.log('hello world!');
 
 const searchForm = document.querySelector('#locationSearch');
@@ -14,12 +14,12 @@ searchForm.addEventListener('submit', (event)=>{
 async function getData(locationString){
     const apiFetch = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationString}?unitGroup=metric&key=9VKUDM7Q8M8GXRWBKGD9F9MMZ&contentType=json`, {mode: 'cors'});
     const data = await apiFetch.json(); 
-    //console.log(data);
+    console.log(data);
     let daysArray = data.days;
     for (let i = 0; i < daysArray.length; i++){
         //console.log(daysArray[i].tempmax);
     }
-    updateTodayIcon(daysArray[0]); 
+    updateToday(daysArray[0]); 
 }
 
 getData();
